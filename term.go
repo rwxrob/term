5562dff5f20397eb5453daaae53cf3ec3439dfd5
rwxrob <rob@rwx.gg>
2022-03-25 05:26:36 -0400

    Make Prompt* use Printf args

--- term.go
+++ term.go
@@ -226,20 +226,22 @@ func ReadHidden() string {
 }
 
 // Prompt prints the given message if the terminal IsInteractive and
-// reads the string by calling Read.
-func Prompt(msg string) string {
+// reads the string by calling Read. The argument signature is identical
+// and passed to to fmt.Printf().
+func Prompt(form string, args ...any) string {
 	if IsInteractive() {
-		fmt.Print(msg)
+		fmt.Printf(form, args...)
 	}
 	return Read()
 }
 
-// PromptHidden prints the given message if the terminal IsInteractive and
-// reads the string by calling ReadHidden (which does not echo to the
-// screen).
-func PromptHidden(msg string) string {
+// PromptHidden prints the given message if the terminal IsInteractive
+// and reads the string by calling ReadHidden (which does not echo to
+// the screen). The argument signature is identical and passed to to
+// fmt.Printf().
+func PromptHidden(form string, args ...any) string {
 	if IsInteractive() {
-		fmt.Print(msg)
+		fmt.Printf(form, args...)
 	}
 	return ReadHidden()
 }
