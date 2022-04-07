dd72c4726bc4d1af34fe0339fab13a4f4034f931
rwxrob <rob@rwx.gg>
2022-04-07 11:12:26 -0400

    Fix windows WinSize bug

--- winsize_dummy.go
+++ winsize_dummy.go
@@ -1,10 +1,10 @@
 // Copyright 2022 Robert S. Muhlestein
-  // SPDX-License-Identifier: Apache-2.0
-  
+// SPDX-License-Identifier: Apache-2.0
+
 //go:build aix || js || nacl || plan9 || windows || android || solaris
 
 package term
 
 func WinSizeUpdate() {
-	WinSize = WinSizeStruct{80, 24, 100, 100}
+	WinSize = WinSizeStruct{24, 80, 100, 100}
 }
